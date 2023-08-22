1e236563823db0d20a14a1f26d2f1dbccc6dd010
iapposki <konark981999@gmail.com>
2023-08-22 17:37:14 +0530

    implemented dto for auth

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,14 +1,24 @@
-import {Controller, Post} from '@nestjs/common'
+import {Body, Controller, Post} from '@nestjs/common'
 import { AuthService } from './auth.service';
+import { AuthDto } from './dto';
 
 @Controller('auth')
 export class AuthController{
     constructor(private authService: AuthService) {}
 
     @Post('signup')
-    signup(){
+    signup(@Body() dto: AuthDto){
+        console.log({dto})
         return this.authService.signup()
     }
+
+
+    // @Post('signup')
+    // signup(@Body('email') email: string, @Body('password') password: string){
+    //     // could use pipe for validating if the request parameter is of the correct type
+    //     console.log({email, password})
+    //     return this.authService.signup()
+    // }
 
     @Post('signin')
     signin(){
